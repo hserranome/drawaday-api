9290a51599e8e925a4b3786b75fdd3042304eced
hserranome <hendy@hserrano.me>
2024-04-13 02:12:58 +0200

    dev and prod dockerization

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { authConfig } from './config/auth.config';
 
-const ENV = process.env.NODE_ENV;
-
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
       load: [dataSourceConfig, authConfig],
       isGlobal: true,
     }),
