f6d2faaf0a21a801b7ddc70dfad8447db61441e2
hserranome <hendy@hserrano.me>
2024-04-28 12:46:08 +0200

    chore: move dto

--- src/auth/auth.constants.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class SignInDto {
-  @ApiProperty({ minLength: 6, maxLength: 255, format: 'email' })
-  email: string;
-
-  @ApiProperty({ minLength: 6, maxLength: 255 })
-  password: string;
-}
