48b71b337b6a442f336d28747d4d6cfbfde61c7b
hserranome <hendy@hserrano.me>
2024-04-28 23:36:02 +0200

    chore: cleanup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiBearerAuth()
 @Controller('users')
-export class UsersController {
-  @Get('me')
-  findMe(): string {
-    return 'This action returns me cats';
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('all')
-  findAll(): string {
-    return 'This action returns all cats';
-  }
-}
+export class UsersController {}
